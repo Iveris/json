2f3a798e60146788a1a33564bcb7fd017752273e
SuperWarnerMan <WarnerIveris@gmail.com>
2022-01-19 19:36:35 -0800

    added tests for non-existent files and URLs

--- src/test/java/read/ReadParallelStreamLocalFileServiceTest.java
+++ src/test/java/read/ReadParallelStreamLocalFileServiceTest.java
@@ -7,14 +7,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-
-import com.google.gson.JsonSyntaxException;
-import com.google.gson.stream.MalformedJsonException;
 
 import org.junit.jupiter.api.MethodOrderer;
 
@@ -28,12 +23,13 @@ class ReadParallelStreamLocalFileServiceTest {
 
 	private static ReadParallelStreamLocalFileService rs = new ReadParallelStreamLocalFileService();
 	private static String inputName = "src/test/resources/data/read/normalUnprocessedData.json";
+	private static String inputURLName = "https://raw.githubusercontent.com/SuperWarnerMan/json/main/src/test/resources/data/read/normalUnprocessedData.json";
 	private static ReadPOJO rp;
 	private static Map<String, WriteObj> POJOMap = new HashMap<>();
 
 	@Test
 	@Order(1)
-	void testNormalDataResultFileSize() {
+	void testNormalDataResultSize() {
 		rs.getReader(inputName);
 		ReadPOJO rp;
 		while(rs.hasNext()) {
@@ -58,7 +54,25 @@ class ReadParallelStreamLocalFileServiceTest {
 	
 	@Test
 	@Order(3)
-	void testDataWithExtraFields() throws IOException{
+	void testNormalDataResultAccuracyURL() {
+		rs.getReader(inputURLName);
+		ReadPOJO rp;
+		while(rs.hasNext()) {
+			rp = (ReadPOJO) rs.next(ReadPOJO.class);
+			ReadPOJOQueue.add(rp);
+		}
+		while(ReadPOJOQueue.getSize() > 0) {
+			rp = ReadPOJOQueue.remove();
+			POJOMap.put(rp.getPath(), new WriteObj(rp.getUrl(), rp.getSize()));
+		}
+		assertEquals(new WriteObj("http://www.lqe.com/tya",164), POJOMap.get("itkbt"));
+		assertEquals(new WriteObj("http://www.lnn.com/usl",233), POJOMap.get("frh"));
+	}
+	
+	
+	@Test
+	@Order(4)
+	void testDataWithExtraFields() throws IOException {
 		String xtraData = "./src/test/resources/data/read/xtraFields.json";
 		//ignores extra fields and only enters predefined fields
 		rs.getReader(xtraData);
@@ -71,6 +85,43 @@ class ReadParallelStreamLocalFileServiceTest {
 		POJOMap.clear();
 	}
 
+	@Test
+	@Order(5)
+	void testNonExistentFile() {
+		//Make sure queue is empty, then create reader with a non-existent
+		//file and test that it instantiated, that it closed, and that
+		//nothing was written to the queue
+		while(ReadPOJOQueue.getSize() > 0) {
+			ReadPOJOQueue.remove();
+		}
+		Thread noFileHere = new Thread(()-> {
+			rs.getReader("invalidFile.json");
+			assertTrue(ReadPOJOQueue.getIsReceivingInput());
+		});
+		noFileHere.start();
+		assertEquals(0, ReadPOJOQueue.getSize());
+		assertFalse(ReadPOJOQueue.getIsReceivingInput());
+	}
+	
+	@Test
+	@Order(6)
+	void testNonExistentURL() {
+		//Make sure queue is empty, then create reader with a non-existent
+		//URL and test that it instantiated, that it closed, and that
+		//nothing was written to the queue
+		while(ReadPOJOQueue.getSize() > 0) {
+			ReadPOJOQueue.remove();
+		}
+		Thread noURLHere = new Thread(()-> {
+			rs.getReader("http://www.github.com/SuperWarnerMan/invalidFile.json");
+			assertTrue(ReadPOJOQueue.getIsReceivingInput());
+		});
+		noURLHere.start();
+		assertEquals(0, ReadPOJOQueue.getSize());
+		assertFalse(ReadPOJOQueue.getIsReceivingInput());
+	}
+	
+	
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
 		rs.closeReader();
