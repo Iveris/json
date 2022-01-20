d68b39650fbe005c7fbb902247339108a9071c2b
SuperWarnerMan <WarnerIveris@gmail.com>
2022-01-19 19:38:31 -0800

    got rid of unneeded setter methods

--- src/main/java/services/read/ReadPOJO.java
+++ src/main/java/services/read/ReadPOJO.java
@@ -16,20 +16,11 @@ public class ReadPOJO {
 	public String getUrl() {
 		return url;
 	}
-	public void setUrl(String url) {
-		this.url = url;
-	}
 	public String getPath() {
 		return path;
 	}
-	public void setPath(String path) {
-		this.path = path;
-	}
 	public int getSize() {
 		return size;
-	}
-	public void setSize(int size) {
-		this.size = size;
 	}
 	
 	@Override
