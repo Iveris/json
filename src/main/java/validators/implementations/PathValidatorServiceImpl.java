704c15be22b6dbadedc97927cd222861175828a1
SuperWarnerMan <WarnerIveris@gmail.com>
2022-01-19 18:17:48 -0800

    created a regex to prevent corrupt data from being processed

--- src/main/java/validators/implementations/PathValidatorServiceImpl.java
+++ src/main/java/validators/implementations/PathValidatorServiceImpl.java
@@ -1,12 +1,19 @@
 package validators.implementations;
 
+import java.util.regex.Pattern;
+
 import validators.services.ValidatorService;
 
 public class PathValidatorServiceImpl implements ValidatorService<String> {
 
+	private String pattern = "[\\w\\d\\.\\_\\/]+";
+	
 	@Override
 	public boolean isValid(String pathField) {
-		return (pathField != null && !pathField.isBlank());
+		
+		return (pathField != null && 
+				!pathField.isBlank() &&
+				Pattern.matches(pattern, pathField));
 	}
 
 }
