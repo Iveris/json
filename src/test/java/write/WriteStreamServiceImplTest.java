85c7d75c577132a33eec1aba15e370a7287f86a6
SuperWarnerMan <WarnerIveris@gmail.com>
2022-01-19 18:42:07 -0800

    fixed file size test

--- src/test/java/write/WriteStreamServiceImplTest.java
+++ src/test/java/write/WriteStreamServiceImplTest.java
@@ -3,11 +3,14 @@ package write;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
@@ -20,9 +23,10 @@ import services.write.WriteStreamServiceImpl;
 class WriteStreamServiceImplTest {
 
 	private static WriteService ws = new WriteStreamServiceImpl();
-	private static String outputFileName = "src/test/resources/data/temporary/processedDataTest.json";
+	private static String actual = "src/test/resources/data/temporary/actual.json";
+	private static String expected = "src/test/resources/data/write/processedDataTest.json";
 	private static Thread t = new Thread(() -> { ws.write(ReadPOJO.class); });
-	private static File outputFile = new File(outputFileName);
+	private static File outputFile = new File(actual);
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -30,20 +34,29 @@ class WriteStreamServiceImplTest {
 		ReadPOJOQueue.add(rp);
 		rp = new ReadPOJO("http://www.lqe.com/tya", "itkbt", 164);
 		ReadPOJOQueue.add(rp);
-		ws.getWriter(outputFileName);
+		ws.getWriter(actual);
 		t.start();
 		ReadPOJOQueue.setIsReceivingInput(false);
 	}
-
+	
 	@Test
-	@Order(1)
 	void testFileSize() {
-		long expectedSize = 166;
-		assertEquals(expectedSize, outputFile.length());
+		Path actualFile = Paths.get(actual);
+		Path expectedFile = Paths.get(expected);
+		long actualSize = 0;
+		long expectedSize = 0;
+		try {
+			actualSize = Files.size(actualFile);
+			expectedSize = Files.size(expectedFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertEquals(expectedSize, actualSize);
 	}
+	
+	
 
 	@AfterAll
-	@Order(2) //file was deleting before testFileSize() was complete, so had to use @Order
 	static void tearDownAfterClass() throws Exception {
 		outputFile.delete();
 	}
