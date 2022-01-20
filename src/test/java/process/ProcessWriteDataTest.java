820c1adee0685ca619cd7a4d9ffd91c65e83d619
SuperWarnerMan <WarnerIveris@gmail.com>
2022-01-19 19:01:36 -0800

    improved file size test and removed unused imports

--- src/test/java/process/ProcessWriteDataTest.java
+++ src/test/java/process/ProcessWriteDataTest.java
@@ -3,6 +3,10 @@ package process;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,8 +17,9 @@ import services.read.ReadPOJO;
 class ProcessWriteDataTest {
 
 	private static ProcessData pd;
-	private static String outputFileName = "src/test/resources/data/temporary/processedDataTest.json";
-	private static File file;
+	private static String actual = "src/test/resources/data/temporary/processWriteDataTest.json";
+	private static String expected = "src/test/resources/data/write/processedDataTest.json";
+	private static File file = new File(actual);
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -23,7 +28,7 @@ class ProcessWriteDataTest {
 		rp = new ReadPOJO("http://www.lnn.com/usl", "frh", 233);
 		ReadPOJOQueue.add(rp);
 		
-		pd = new ProcessWriteData(outputFileName);
+		pd = new ProcessWriteData(actual);
 		Thread t = new Thread(pd);
 		t.start();
 		ReadPOJOQueue.setIsReceivingInput(false);
@@ -31,9 +36,17 @@ class ProcessWriteDataTest {
 
 	@Test
 	void testFileSizes() throws Exception {
-		long expectedSize = 166;
-		file = new File(outputFileName);
-		assertEquals(expectedSize, file.length());
+		Path actualFile = Paths.get(actual);
+		Path expectedFile = Paths.get(expected);
+		long actualSize = 0;
+		long expectedSize = 0;
+		try {
+			actualSize = Files.size(actualFile);
+			expectedSize = Files.size(expectedFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertEquals(expectedSize, actualSize);
 	}
 
 	@AfterAll
